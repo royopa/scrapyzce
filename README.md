34116f6849bcbd3599b9d97c65c18a62fa072462
Rodrigo Prado de Jesus <royopa@gmail.com>
2015-07-21 09:16:58 -0300

    added item

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # scrapyzce
+
+
+http://doc.scrapy.org/en/latest/intro/tutorial.html
+
+http://www.zend.com/en/yellow-pages/ZEND024088
--- scrapyzce/items.py
+++ scrapyzce/items.py
@@ -10,5 +10,13 @@ import scrapy
 
 class ScrapyzceItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
+    candidate_name = scrapy.Field()
+    cert_name = scrapy.Field()
+    candidate_id = scrapy.Field()
+    cert_date = scrapy.Field()
+    company_name = scrapy.Field()
+    city = scrapy.Field()
+    state_region = scrapy.Field()
+    country = scrapy.Field()
+    links = scrapy.Field()
     pass
